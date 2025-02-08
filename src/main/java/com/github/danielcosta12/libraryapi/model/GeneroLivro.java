bbbe5e7fe27bca52dad6050885d33326b2a8359a
Daniel Costa <danieldossantosdacosta@gmail.com>
2025-02-08 18:25:45 -0300

    add string dos generos

--- src/main/java/com/github/danielcosta12/libraryapi/model/GeneroLivro.java
+++ src/main/java/com/github/danielcosta12/libraryapi/model/GeneroLivro.java
@@ -1,4 +1,14 @@
 package com.github.danielcosta12.libraryapi.model;
 
 public enum GeneroLivro {
+    FICCAO,
+    NAO_FICCAO,
+    FANTASIA,
+    FICCAO_CIENTIFICA,
+    MISTERIO,
+    ROMANCE,
+    TERROR,
+    AVENTURA,
+    BIOGRAFIA,
+    HISTORIA
 }
