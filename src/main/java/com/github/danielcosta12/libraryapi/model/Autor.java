ec285c72b709677980b31a2ac023b78f218fab03
Daniel Costa <danieldossantosdacosta@gmail.com>
2025-02-16 18:14:10 -0300

    adicionando auditoria jpa nas entidades, adicionando novas colunas e evento para ficar ouvindo se mudar horario/atualizacao

--- src/main/java/com/github/danielcosta12/libraryapi/model/Autor.java
+++ src/main/java/com/github/danielcosta12/libraryapi/model/Autor.java
@@ -3,15 +3,20 @@ package com.github.danielcosta12.libraryapi.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 @Entity
 @Data
 @ToString
 @Table(name = "autor", schema = "public")
+@EntityListeners(AuditingEntityListener.class) //vai ficar escutando para jogar as datas no @CreatedDate e @LastModifiedDate
 public class Autor {
    @Id
    @Column(name = "id")
@@ -31,4 +36,14 @@ public class Autor {
    @OneToMany(mappedBy = "autor") //, cascade = CascadeType.ALL) //um autor para muitos livros
    //@Transient
    private List<Livro> livros;
+
+   @CreatedDate
+   @Column(name = "data_cadastro")
+   private LocalDateTime dataCadastro;
+   @LastModifiedDate
+   @Column(name = "data_atualizacao")
+   private LocalDateTime dataAtualizacao;
+   @Column(name = "id_usuario")
+   private UUID idUsuario;
+
 }
