265f05c6d7975e415531bdc4f12de4a87e8a675d
Daniel Costa <danieldossantosdacosta@gmail.com>
2025-02-15 21:59:30 -0300

    add api e DTO

--- src/main/java/com/github/danielcosta12/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/com/github/danielcosta12/libraryapi/controller/dto/AutorDTO.java
@@ -1,4 +1,16 @@
 package com.github.danielcosta12.libraryapi.controller.dto;
 
-public record AutorDTO() {
+import com.github.danielcosta12.libraryapi.model.Autor;
+
+import java.time.LocalDate;
+
+public record AutorDTO(String nome, LocalDate dataNascimento, String nacionalidade) {
+
+    public Autor mapearParaAutor(){
+        Autor autor = new Autor();
+        autor.setNome(this.nome);
+        autor.setDataNascimento(this.dataNascimento);
+        autor.setNacionalidade(this.nacionalidade);
+        return autor;
+    }
 }
